9c98865b9d8646373e8ecee331119124ae63db99
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2021-03-25 13:53:28 -0500

    Update model version for Stampede
    Update the coupled model version to a more recent one that has
    Stampede support.
    
    Refs: #280

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -42,7 +42,7 @@ else
   if [[ ! -d ufs_coupled.fd ]] ; then
     git clone https://github.com/ufs-community/ufs-weather-model ufs_coupled.fd >> ${topdir}/checkout-ufs_coupled.log 2>&1
     cd ufs_coupled.fd
-    git checkout 3e46f5b7050e18884a0bed13691823ad88d443c3 
+    git checkout 37f0da7b82e32a15bf072eaa158399a6b0f2722c
     git submodule update --init --recursive
     cd ${topdir} 
   else 
